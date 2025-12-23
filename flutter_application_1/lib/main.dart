33884a392827e92dd29d757b400d5c93dff0de17
MOHANSAIBUCHIREDDYGARI <cb.sc.u4cse23409@cb.students.amrita.edu>
2025-12-24 00:40:42 +0530

    Add files via upload

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,29 +1,19 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'features/product_catalog/presentation/screens/home_screen.dart';
 
-// Global Notifier to control the theme
-final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.dark);
+void main() => runApp(const CatalogApp());
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CatalogApp extends StatelessWidget {
+  const CatalogApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<ThemeMode>(
-      valueListenable: themeNotifier,
-      builder: (_, mode, __) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: mode, // This allows the toggle to work
-          home: const HomeScreen(),
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Premium Product Catalog',
+      theme: AppTheme.lightTheme,
+      home: const HomeScreen(),
     );
   }
 }
